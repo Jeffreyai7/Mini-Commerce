db9733eaf71398295d0d6400b2d6ccc27a5360ff
jeffreyai7 <jirukeh@gmail.com>
2025-07-04 16:19:20 +0100

    update the local database

---
+++ src/lib/constants.ts
@@ -0,0 +1,8 @@
+const navLinks = [
+  { name: 'Home', href: '/' },
+  { name: 'Shop', href: '/shop' },
+  { name: 'Cart', href: '/cart' },
+  { name: 'Account', href: '/account' }
+];
+
+export default navLinks;
